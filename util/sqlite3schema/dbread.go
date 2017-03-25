8ea2deeb3c351d2024ff26bc076683af1b284064
Gimpl do foo <gimpldo@gmail.com>
2017-03-25 15:05:01 +0200

    Should not have a 'types.go' file in packages under 'util'; move the struct type to the file containing the utility function.

--- util/sqlite3schema/dbread.go
+++ util/sqlite3schema/dbread.go
@@ -9,6 +9,19 @@ import (
 	"github.com/gimpldo/ba-prototype-go/sqlschema"
 )
 
+// ElementFound = SQLite database schema element found (after a search).
+// Schema elements are also known as "schema objects".
+type ElementFound struct {
+	ElemType sqlschema.ElemTypeCode
+
+	// page number -1 means that the rootpage column was NULL in 'sqlite_master'
+	RootPage int32
+
+	Name      string
+	TableName string
+	CreateSQL string
+}
+
 func ReadFromDB(db *sql.DB, namePrefix, nameSuffix string) ([]ElementFound, error) {
 	var err error
 	err = checkSafeNameChars(namePrefix)
